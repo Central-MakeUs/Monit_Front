748e4077b5f2f8fd8e191d934ca983b79c7a3eb0
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-01-31 18:56:24 +0900

    chore: svg 파일 추가

--- apps/web/src/shared/ui/tooltip/Tooltip.tsx
+++ apps/web/src/shared/ui/tooltip/Tooltip.tsx
@@ -1,5 +1,6 @@
 import * as styles from './Tooltip.css';
 import { Text, vars } from '@/shared/ui';
+import { IcToolTip } from 'public/icons';
 import React from 'react';
 
 export interface TooltipProps {
@@ -17,15 +18,6 @@ export interface TooltipProps {
   className?: string;
 }
 
-const ArrowIcon = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='20' height='10' viewBox='0 0 16 10' fill='none'>
-    <path
-      d='M0.491421 2.84518L6.46773 8.82149C7.1186 9.47236 8.17388 9.47236 8.82475 8.82149L14.8011 2.84518C15.851 1.79524 15.1074 0 13.6226 0L1.66993 2.42773e-06C0.185088 2.65336e-06 -0.558521 1.79524 0.491421 2.84518Z'
-      fill='white'
-    />
-  </svg>
-);
-
 export const Tooltip = ({
   arrow = 'left',
   direction = 'bottom',
@@ -37,7 +29,7 @@ export const Tooltip = ({
   const arrowElement = (
     <div className={styles.arrowContainerVariants[direction]}>
       <div className={direction === 'top' ? styles.arrowFlipped : undefined}>
-        <ArrowIcon />
+        <IcToolTip color='#ffff' />
       </div>
     </div>
   );
