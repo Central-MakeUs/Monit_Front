77aa7f0208020a1b8888e374bbffa359d6fe8998
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-02 17:12:51 +0900

    chore: ExpensePage 이름 수정

--- apps/web/app/expense/page.tsx
+++ apps/web/app/expense/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { ExpenseRecordFunnel } from '@/widgets/expenseRecordFunnel';
 
-export default function ExpenseRecordPage(): React.ReactElement | null {
+export default function ExpensePage(): React.ReactElement | null {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
