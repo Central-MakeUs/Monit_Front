46ee97757bdb79a386fef082a6d7d01d9025ecbd
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-04 00:51:03 +0900

    refactor: PageIndicator 사이즈 추가

--- apps/web/src/shared/ui/pageIndicator/PageIndicator.tsx
+++ apps/web/src/shared/ui/pageIndicator/PageIndicator.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { container, dot } from './PageIndicator.css';
 
 export interface PageIndicatorProps {
+  size?: 'sm' | 'md';
   currentPage: number;
   totalPages: number;
 }
 
-export const PageIndicator = ({ currentPage, totalPages }: PageIndicatorProps) => {
+export const PageIndicator = ({ size = 'sm', currentPage, totalPages }: PageIndicatorProps) => {
   return (
-    <div className={container}>
+    <div className={container({ size })}>
       {Array.from({ length: totalPages }, (_, index) => (
         <div
           key={index}
           className={dot({
             state: index === currentPage ? 'active' : 'default',
+            size,
           })}
         />
       ))}
