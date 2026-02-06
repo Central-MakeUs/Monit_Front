66b8df8f9152e9d6982a7ee725e021dd1b060b48
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-06 15:11:13 +0900

    feat: 로그아웃/회원탈퇴 모달,토스트 추가

--- apps/web/src/widgets/myPage/ui/MyPage.tsx
+++ apps/web/src/widgets/myPage/ui/MyPage.tsx
@@ -1,17 +1,41 @@
 'use client';
 
 import { MenuItem, MenuLabel } from '@/features/my';
-import { TopBar, vars, Text, Divider } from '@/shared/ui';
+import { TopBar, vars, Text, Divider, AlertDialog, useToast } from '@/shared/ui';
 import { IcLeftChevron } from 'public/icons';
 import React from 'react';
 import * as styles from './myPage.css';
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/shared/hooks';
 
 export const MyPage = () => {
   const router = useRouter();
+  const toast = useToast();
+  const {
+    isOpen: isLogoutOpen,
+    openModal: openLogoutModal,
+    closeModal: closeLogoutModal,
+  } = useModal();
+  const {
+    isOpen: isWithdrawOpen,
+    openModal: openWithdrawModal,
+    closeModal: closeWithdrawModal,
+  } = useModal();
+
   // TODO: 페이지 만들면 router 아래 모두 수정
-  // TODO: 이용 동의 API 연동
-  // TODO: 로그아웃, 탈퇴 모달 창
+  // TODO: 알림 설정
+  const handleLogout = () => {
+    // TODO: 로그아웃 API 연동
+    toast.success('로그아웃이 완료되었어요');
+    router.push('/');
+  };
+
+  const handleWithdraw = () => {
+    // TODO: 회원탈퇴 API 연동
+    toast.success('회원탈퇴가 완료되었어요');
+    router.push('/');
+  };
+
   return (
     <div>
       <TopBar
@@ -50,9 +74,31 @@ export const MyPage = () => {
         {/* 계정 설정 */}
         <MenuLabel label='계정 설정' />
         <MenuItem type='arrow' label='1:1문의' onClick={() => router.push('/my/support')} />
-        <MenuItem label='로그아웃' />
-        <MenuItem label='회원탈퇴' />
+        <MenuItem label='로그아웃' onClick={openLogoutModal} />
+        <MenuItem label='회원탈퇴' onClick={openWithdrawModal} />
       </div>
+      <AlertDialog
+        isOpen={isLogoutOpen}
+        onClose={closeLogoutModal}
+        variant='left'
+        title='로그아웃 하시겠어요?'
+        description='로그인 화면으로 돌아가요.'
+        cancelText='취소'
+        confirmText='로그아웃'
+        onCancel={closeLogoutModal}
+        onConfirm={handleLogout}
+      />
+      <AlertDialog
+        isOpen={isWithdrawOpen}
+        onClose={closeWithdrawModal}
+        variant='left'
+        title='정말 탈퇴하시겠어요?'
+        description={`작성한 소비 기록이 모두 삭제되며,\n한 번 삭제된 데이터는 다시 복구할 수 없어요.`}
+        cancelText='취소'
+        confirmText='탈퇴'
+        onCancel={closeWithdrawModal}
+        onConfirm={handleWithdraw}
+      />
     </div>
   );
 };
