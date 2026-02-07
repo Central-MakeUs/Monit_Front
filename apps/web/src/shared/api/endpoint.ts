3dacd3e31b360ef0da28e80e4d55ae0fd2c82b08
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-08 01:01:46 +0900

    chore: endpoint / 제거

--- apps/web/src/shared/api/endpoint.ts
+++ apps/web/src/shared/api/endpoint.ts
@@ -1,35 +1,29 @@
-import type { paths } from '../api/schema';
-
-type Endpoint = keyof paths;
-
-const endpoint = <T extends Endpoint>(path: T): T => path;
-
 export const ENDPOINT = {
   // Auth
   AUTH: {
-    KAKAO_CALLBACK: endpoint('/api/auth/kakao/callback'),
-    APPLE_LOGIN: endpoint('/api/auth/apple/login'),
-    APPLE_CALLBACK: endpoint('/api/auth/apple/callback'),
-    REISSUE: endpoint('/api/auth/reissue'),
-    LOGOUT: endpoint('/api/auth/logout'),
-    WITHDRAW: endpoint('/api/auth/withdraw'),
+    KAKAO_CALLBACK: 'api/auth/kakao/callback',
+    APPLE_LOGIN: 'api/auth/apple/login',
+    APPLE_CALLBACK: 'api/auth/apple/callback',
+    REISSUE: 'api/auth/reissue',
+    LOGOUT: 'api/auth/logout',
+    WITHDRAW: 'api/auth/withdraw',
   },
   // Expense
   EXPENSE: {
-    RECORD: endpoint('/api/expense/record'),
-    UPDATE_RECORD: endpoint('/api/expense/update_record/{expenseId}'),
-    REMIND: endpoint('/api/expense/remind'),
-    DAILY: endpoint('/api/expense/daily'),
-    RETROSPECT_LIST: endpoint('/api/expense/retrospect-list'),
+    RECORD: 'api/expense/record',
+    UPDATE_RECORD: 'api/expense/update_record/{expenseId}',
+    REMIND: 'api/expense/remind',
+    DAILY: 'api/expense/daily',
+    RETROSPECT_LIST: 'api/expense/retrospect-list',
   },
   EXPENSE_REPORT: {
-    WEEKLY_DETAIL: endpoint('/api/expense/weekly_detail'),
-    SUMMARY_RECORD: endpoint('/api/expense/summary_record'),
-    DAILY_SATISFACTION: endpoint('/api/expense/daily_satisfaction'),
+    WEEKLY_DETAIL: 'api/expense/weekly_detail',
+    SUMMARY_RECORD: 'api/expense/summary_record',
+    DAILY_SATISFACTION: 'api/expense/daily_satisfaction',
   },
   CATEGORY: {
-    CATEGORY_LIST: endpoint('/api/expense/category_list'),
-    CATEGORY_CREATE: endpoint('/api/expense/category_create'),
-    CATEGORY_UPDATE: endpoint('/api/expense/category_update/{categoryId}'),
+    CATEGORY_LIST: 'api/expense/category_list',
+    CATEGORY_CREATE: 'api/expense/category_create',
+    CATEGORY_UPDATE: 'api/expense/category_update/{categoryId}',
   },
 } as const;
