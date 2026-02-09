c62a7f27e311dd700a5961e875f72a61f2c72a3a
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-09 19:59:04 +0900

    feat: 1000자 이상일경우 오류 띄우기

--- apps/web/src/widgets/myPage/ui/MyPage.tsx
+++ apps/web/src/widgets/myPage/ui/MyPage.tsx
@@ -67,7 +67,8 @@ export const MyPage = () => {
           label='개인정보 수집 및 이용 동의'
           onClick={() => window.open(EXTERNAL_URLS.PRIVACY_POLICY, '_blank')}
         />
-        <MenuItem type='toggle' label='알림 설정 허용' />
+        {/*  */}
+        {/* <MenuItem type='toggle' label='알림 설정 허용' /> */}
         <div className={styles.dividerWrapper}>
           <Divider />
         </div>
