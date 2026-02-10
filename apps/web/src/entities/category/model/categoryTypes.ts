e5b8970293120547b2785731eea1b75b18234252
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-10 20:33:35 +0900

    feat: 카테고리 Type 정의

--- apps/web/src/entities/category/model/categoryTypes.ts
+++
@@ -1,6 +0,0 @@
-import { components } from '@/shared/api/schema';
-
-//TODO: 서버 id를 required로 변경 요청
-export type CategoryListResponseDTO = components['schemas']['CategoryListResponseDTO'] & {
-  id: number;
-};
