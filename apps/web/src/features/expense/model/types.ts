a0fabd0d4b4322bb67b2f0cb2ae161b23b123926
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-10 17:45:25 +0900

    feat: postExpense 구현

---
+++ apps/web/src/features/expense/model/types.ts
@@ -0,0 +1,4 @@
+import { components } from '@/shared/api/schema';
+
+export type ExpenseDetailsDTO = components['schemas']['ExpenseDetailsDTO'];
+export type IdResponse = components['schemas']['IdResponse'];
