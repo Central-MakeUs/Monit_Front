caf8ff8b379822c1c29754a817b9a98774c3b9f8
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2026-02-11 14:07:48 +0900

    fix: 문의api ApiResponse 응답수정

--- apps/web/src/features/inquiry/api/postInquirySend.ts
+++ apps/web/src/features/inquiry/api/postInquirySend.ts
@@ -1,6 +1,6 @@
-import { authApi, ENDPOINT } from '@/shared/api';
+import { ApiResponse, authApi, ENDPOINT } from '@/shared/api';
 import { InquiryRequest } from '../model/type';
 
 export const postInquirySend = async (body: InquiryRequest) => {
-  return await authApi.post<string>(ENDPOINT.INQUIRY.INQUIRY_SEND, body);
+  return await authApi.post<ApiResponse>(ENDPOINT.INQUIRY.INQUIRY_SEND, body);
 };
