8f1d1244eb412828d756b8e18dadc68ffd72ac19
hyun907 <seunghyun020907@naver.com>
2026-03-29 19:44:04 +0900

    chore: report toggle 숨김 처리 해제 및 홈 온보딩 step report 포함으로 변경

--- apps/web/src/features/onboarding/config/steps.home.ts
+++ apps/web/src/features/onboarding/config/steps.home.ts
@@ -7,7 +7,7 @@ export const HOME_STEPS: OnboardingStep[] = [
   {
     key: 'plus-btn',
     title: '소비기록',
-    stepOrder: '(1/2)',
+    stepOrder: '(1/3)',
     desc: '플러스 버튼을 눌러, 소비를 기록해 보세요',
     direction: 'bottom',
     arrow: 'right',
@@ -16,19 +16,19 @@ export const HOME_STEPS: OnboardingStep[] = [
   {
     key: 'banner',
     title: '소비 돌아보기',
-    stepOrder: '(2/2)',
+    stepOrder: '(2/3)',
     desc: '소비를 돌아보며 나의 만족도를 기록할 수 있어요',
     direction: 'top',
     arrow: 'right',
     tooltipOffset: { x: '18.8rem', y: '0rem' },
   },
-  // {
-  //   key: 'nav-toggle',
-  //   title: '소비 분석 리포트',
-  //   stepOrder: '(3/3)',
-  //   desc: '내 소비 만족도를 리포트로 한눈에 볼 수 있어요',
-  //   direction: 'bottom',
-  //   arrow: 'center',
-  //   tooltipOffset: { x: '7.6rem', y: '0rem' },
-  // },
+  {
+    key: 'nav-toggle',
+    title: '소비 분석 리포트',
+    stepOrder: '(3/3)',
+    desc: '내 소비 만족도를 리포트로 한눈에 볼 수 있어요',
+    direction: 'bottom',
+    arrow: 'center',
+    tooltipOffset: { x: '7.6rem', y: '0rem' },
+  },
 ];
--- apps/web/src/shared/ui/bottomNav/BottomNav.css.ts
+++ apps/web/src/shared/ui/bottomNav/BottomNav.css.ts
@@ -12,17 +12,9 @@ export const container = style({
   padding: `${vars.spacing.md} ${vars.spacing.xl}`,
   backgroundColor: 'transparent',
   zIndex: 100,
-
-  // 네비게이션 토글은 2차 배포에서 숨김 처리되어 있기 때문에 터치를 받지 않도록 한다.
-  // TODO: 2차 배포 완료 후 숨김 처리 해제
-  pointerEvents: 'none',
 });
 
 export const plusButtonWrapper = style({
   position: 'absolute',
   right: vars.spacing.xl,
-
-  // 컨테이너에 pointer-events: 'none'을 주었기 때문에
-  // 실제로 클릭 가능한 플러스 버튼 영역만 터치를 받도록 수정
-  pointerEvents: 'auto',
 });
